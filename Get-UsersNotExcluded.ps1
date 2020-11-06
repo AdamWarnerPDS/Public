067fc7134d7f4986527308765e8c7b52e4159391
Adam Warner <awarner@pdsit.net>
2020-11-06 13:24:03 -0600

    Update Get-UsersNotExcluded.ps1

--- Get-UsersNotExcluded.ps1
+++ Get-UsersNotExcluded.ps1
@@ -1,6 +1,12 @@
-$exclusionsPath = '.\ExcludedUsers.csv'
+param (
+    # domain to query, use a FQDN ie: subdomain.contoso.com
+    [Parameter(mandatory=$true)]
+    [string]
+    $domain = ""
+)
 
-$domain = "ampkcorp.com"
+
+$exclusionsPath = '.\ExcludedUsers.csv'
 
 $dateTime = (Get-Date -Format yyyyMMdd-HHmmss )
 $outPath = '.\'
@@ -25,13 +31,15 @@ foreach ( $u in $adUsers ) {
     $outputObject = New-Object psobject
     $outputObject | Add-Member -MemberType NoteProperty -Name "SamAccountName" -Value "blank" -Force
     $outputObject | Add-Member -MemberType NoteProperty -Name "UserPrincipalName" -Value "blank" -Force
-    $outputObject | Add-Member -MemberType NoteProperty -Name "ExcludedBy" -Value "blank" -Force   
+    $outputObject | Add-Member -MemberType NoteProperty -Name "Name" -Value "blank" -Force
+    #$outputObject | Add-Member -MemberType NoteProperty -Name "ExcludedBy" -Value "blank" -Force   
     
-    if ( "$($u.SamAccountName)" -notin "$($exclusions.SamAccountName)" ) {
-        if ( "$($u.UserPrincipalName)" -notin "$($exclusions.UserPrincipalName)") {
+    if ( "$($u.SamAccountName)" -notin $($exclusions.SamAccountName) ) {
+        if ( "$($u.UserPrincipalName)" -notin $($exclusions.UserPrincipalName)) {
             $outputObject.SamAccountName = "$($u.SamAccountName)"
             $outputObject.UserPrincipalName = "$($u.UserPrincipalName)"
-            $outputObject.ExcludedBy = "$($exclusions.source)"
+            $outputObject.Name = "$($u.Name)"
+            #$outputObject.ExcludedBy = "$($exclusions.source)"
 
             $includedUsers = $includedUsers + $outputObject
         }
@@ -39,3 +47,5 @@ foreach ( $u in $adUsers ) {
 }
 
 $includedUsers | Export-CSV -Path "$outPath$outName" -NoTypeInformation
+
+
