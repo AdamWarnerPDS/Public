c777c255aa9174caf7d4579e5eb5d4dbe95b5e98
Adam Warner <awarner@pdsit.net>
2021-05-03 17:07:30 -0500

    Create Cleanup-CrucibleFisheyeBackups.sh
    initial commit

---
+++ Cleanup-CrucibleFisheyeBackups.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+## This script cleans up the folder at $path, deleting old files until it is equal to or smaller than $size in Megabytes
+## Created to cleanup atlassian backups, but can do other things
+## Created by Adam Warner
+
+# Size to shrink folder to in MB
+targetSize=8192
+
+# Path to target
+path='/usr/lib/atlassian/backup'
+
+# Gets the current size, don't change this
+size=$(du -ms "$path" | awk '{print $1}')
+
+# loops by deleting oldest file until target is reached
+while [ $size -ge $targetSize ] 
+    do 
+        oldestFile=$(find $path -type f -printf '%T+ %p\n' | sort | head -n 1 | awk '{print $2}')
+        rm $oldestFile
+        size=$(du -ms "$path" | awk '{print $1}')
+done
