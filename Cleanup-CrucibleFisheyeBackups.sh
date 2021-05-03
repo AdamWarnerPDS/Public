81fb369ec9e85ca6c96ff755fd75772688a602af
Adam Warner <awarner@pdsit.net>
2021-05-03 17:13:58 -0500

    Update Cleanup-CrucibleFisheyeBackups.sh
    Simplify find oldest file

--- Cleanup-CrucibleFisheyeBackups.sh
+++ Cleanup-CrucibleFisheyeBackups.sh
@@ -16,7 +16,7 @@ size=$(du -ms "$path" | awk '{print $1}')
 # loops by deleting oldest file until target is reached
 while [ $size -ge $targetSize ] 
     do 
-        oldestFile=$(find $path -type f -printf '%T+ %p\n' | sort | head -n 1 | awk '{print $2}')
+        oldestFile=$(find $path -type f -printf '%p\n' | sort | head -n 1
         rm $oldestFile
         size=$(du -ms "$path" | awk '{print $1}')
 done
