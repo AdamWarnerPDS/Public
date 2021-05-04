29c0e0d5aa30e66141479ec9da78f1dd89df5c6a
Adam Warner <awarner@pdsit.net>
2021-05-04 11:43:51 -0500

    more tidy

--- Cleanup-CrucibleFisheyeBackups.sh
+++ Cleanup-CrucibleFisheyeBackups.sh
@@ -5,18 +5,19 @@
 ## Created by Adam Warner
 
 # Size to shrink folder to in MB
-targetSize=8192
+targetSize=10240
 
 # Path to target
 path='/usr/lib/atlassian/backup'
 
-# Gets the current size, don't change this
+# Gets the current size in MB, don't change this
 size=$(du -ms "$path" | awk '{print $1}')
 
 # loops by deleting oldest file until target is reached
 while [ $size -ge $targetSize ] 
-    do 
-        oldestFile=$(find $path -type f -printf '%T+ %p\n' | sort | head -n 1 | awk '{print $2}')
-        rm $oldestFile
-        size=$(du -ms "$path" | awk '{print $1}')
+do 
+    # leave the %T for sorting, though it seems extraneous, it's good for other items if we reuse this
+    oldestFile=$(find $path -type f -printf '%T+ %p\n' | sort | head -n 1 | awk '{print $2}')
+    rm "$oldestFile"
+    size=$(du -ms "$path" | awk '{print $1}')
 done
